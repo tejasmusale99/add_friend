0d3d9f3bec428ec66f204d27201858164efc50bf
Tejas Musale <tejasmusale99@gmail.com>
2025-03-27 22:41:07 +0530

    combined logic for friend unfriend

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const addFriend = document.querySelector('#addFriend_btn')
-const removeFriend = document.querySelector('#removeFriend_btn')
+const addFriend = document.querySelector("#addFriend_btn");
+// const removeFriend = document.querySelector('#removeFriend_btn')
 
-const addedConfirmation = document.querySelector('#msgConfirm')
+const addedConfirmation = document.querySelector("#msgConfirm");
 
-addFriend.addEventListener("click", function() {
+let value = 0;
+
+addFriend.addEventListener("click", function () {
+  if (value == 0) {
     addedConfirmation.innerHTML = "Friends";
-    addedConfirmation.style.color = "green"
-  });
+    addedConfirmation.style.color = "green";
+    addFriend.innerHTML = "Removed Friend"
+    value = 1;
+  }else{
+    addedConfirmation.innerHTML = "Stranger";
+    addedConfirmation.style.color = "red";
+    value = 0;
+    addFriend.innerHTML = "Add Friend"
+  }
 
-  removeFriend.addEventListener('click',function(){
-    addedConfirmation.innerHTML = "Stranger"
-    addedConfirmation.style.color = "red"
-  })
+});
+
+//   removeFriend.addEventListener('click',function(){
+//     addedConfirmation.innerHTML = "Stranger"
+//     addedConfirmation.style.color = "red"
+//   })
